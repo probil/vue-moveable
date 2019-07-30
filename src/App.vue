eab062e8578eda44762a1be38a72c1c5cc5532a4
Max Lyashuk <m_lyashuk@ukr.net>
2019-07-31 01:15:05 +0300

    docs: move badges to the separate component

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         @rotate="handleRotate"
         @warp="handleWarp"
       >
-        <span>Moveable</span>
+        <span>Vue Moveable</span>
       </Moveable>
       <div class="buttons able">
         <a
@@ -21,30 +21,7 @@
           @click="currentState = key"
         >{{ state }}</a>
       </div>
-      <p class="badges">
-        <a href="https://www.npmjs.com/package/moveable" target="_blank">
-          <img src="https://img.shields.io/npm/v/vue-moveable.svg?style=flat-square&color=007acc&label=version"
-               alt="npm version" /></a>
-        <a href="https://github.com/probil/vue-moveable" target="_blank">
-          <img
-            src="https://img.shields.io/github/stars/probil/vue-moveable.svg?color=42b883&style=flat-square"
-            alt="github stars"/></a>
-        <a href="https://github.com/probil/vue-moveable/blob/master/LICENSE" target="_blank">
-          <img
-            src="https://img.shields.io/github/license/probil/vue-moveable.svg?style=flat-square&label=license&color=08CE5D"
-            alt="license"
-          />
-        </a>
-        <a href="https://github.com/daybrush/moveable/tree/master/packages/react-moveable" target="_blank"><img
-          alt="React"
-          src="https://img.shields.io/static/v1.svg?label=&message=React&style=flat-square&color=61daeb"></a>
-        <a href="https://github.com/daybrush/moveable/tree/master/packages/preact-moveable" target="_blank"><img
-          alt="React"
-          src="https://img.shields.io/static/v1.svg?label=&message=Preact&style=flat-square&color=673ab8"></a>
-        <a href="https://github.com/probil/vue-moveable" target="_blank"><img
-          alt="React"
-          src="https://img.shields.io/static/v1.svg?label=&message=Vue&style=flat-square&color=3fb984"></a>
-      </p>
+      <Badges />
       <p class="description">Moveable is Draggable! Resizable! Scalable! Rotatable!</p>
       <div class="buttons">
         <a href="https://github.com/probil/vue-moveable" target="_blank">Download</a>
@@ -57,11 +34,13 @@
 <script>
 /* eslint-disable no-param-reassign,no-unused-expressions */
 import Moveable from '@/components/Moveable.vue';
+import Badges from '@/components/Badges.vue';
 
 export default {
   name: 'app',
   components: {
     Moveable,
+    Badges,
   },
   data: () => ({
     moveable: {
@@ -163,7 +142,7 @@ export default {
   .moveable {
     font-family: "Roboto", sans-serif;
     position: relative;
-    width: 250px;
+    width: 300px;
     height: 200px;
     text-align: center;
     font-size: 40px;
@@ -214,16 +193,5 @@ export default {
   .buttons a:hover, .buttons a.selected {
     background: #333;
     color: #fff;
-  }
-
-  .badges {
-    text-align: center;
-  }
-  .badges a{
-    margin: 0 3px;
-  }
-
-  .badges img {
-    height: 20px;
   }
 </style>
---
+++ src/components/Badges.vue
@@ -0,0 +1,37 @@
+<template>
+  <p class="badges">
+    <a href="https://www.npmjs.com/package/moveable" target="_blank">
+      <img src="https://img.shields.io/npm/v/vue-moveable.svg?style=flat-square&color=007acc&label=version"
+           alt="npm version" /></a>
+    <a href="https://github.com/probil/vue-moveable" target="_blank">
+      <img
+        src="https://img.shields.io/github/stars/probil/vue-moveable.svg?color=42b883&style=flat-square"
+        alt="github stars"/></a>
+    <a href="https://github.com/probil/vue-moveable/blob/master/LICENSE" target="_blank">
+      <img
+        src="https://img.shields.io/github/license/probil/vue-moveable.svg?style=flat-square&label=license&color=08CE5D"
+        alt="license"
+      />
+    </a>
+    <a href="https://github.com/daybrush/moveable/tree/master/packages/react-moveable" target="_blank"><img
+      alt="React"
+      src="https://img.shields.io/static/v1.svg?label=&message=React&style=flat-square&color=61daeb"></a>
+    <a href="https://github.com/daybrush/moveable/tree/master/packages/preact-moveable" target="_blank"><img
+      alt="React"
+      src="https://img.shields.io/static/v1.svg?label=&message=Preact&style=flat-square&color=673ab8"></a>
+    <a href="https://github.com/probil/vue-moveable" target="_blank"><img
+      alt="React"
+      src="https://img.shields.io/static/v1.svg?label=&message=Vue&style=flat-square&color=3fb984"></a>
+  </p>
+</template>
+<style>
+  .badges {
+    text-align: center;
+  }
+  .badges a {
+    margin: 0 3px;
+  }
+  .badges img {
+    height: 20px;
+  }
+</style>

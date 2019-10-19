9d44b948ca86ce6a442c002df8aaba297f01ed3c
Max Lyashuk <m_lyashuk@ukr.net>
2019-10-19 13:06:24 +0300

    feat(bundler): don't include global polyfills for library

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
+const isLib = process.env.VUE_CLI_BUILD_TARGET === 'lib';
+
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    ['@vue/cli-plugin-babel/preset', {
+      useBuiltIns: isLib
+        ? false
+        : 'usage',
+    }],
   ],
 };

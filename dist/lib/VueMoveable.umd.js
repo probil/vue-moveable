0ae8c91018085b3b6322e0f105b33827f5a851f8
Max Liashuk <m_lyashuk@ukr.net>
2020-04-29 12:39:57 +0300

    build: build 1.7.2

--- dist/lib/VueMoveable.umd.js
+++ dist/lib/VueMoveable.umd.js
@@ -205,12 +205,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"43f4d438-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Moveable.vue?vue&type=template&id=5632c640&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"10648969-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Moveable.vue?vue&type=template&id=65780c75&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_vm._t("default")],2)}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/Moveable.vue?vue&type=template&id=5632c640&
+// CONCATENATED MODULE: ./src/components/Moveable.vue?vue&type=template&id=65780c75&
 
 // CONCATENATED MODULE: ./node_modules/@egjs/component/dist/component.esm.js
 /*
@@ -12010,7 +12010,12 @@ METHODS.forEach(function (name) {
       target: this.$el
     }));
     EVENTS.forEach(function (event) {
-      return _this.moveable.on(event, _this.$emit.bind(_this, event));
+      var kebabCaseEvent = event.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+
+      _this.moveable.on(event, _this.$emit.bind(_this, kebabCaseEvent)); // Backwards support for camelCase events
+
+
+      _this.moveable.on(event, _this.$emit.bind(_this, event));
     });
     window.addEventListener('resize', this.updateRect, {
       passive: true
